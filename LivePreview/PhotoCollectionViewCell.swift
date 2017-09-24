132007ead8a0a17ffd49f61574aad8ff22aae2c3
Tanja Keune <tanjakeune@gmail.com>
2017-09-23 22:18:12 -0400

    LivePreview

---
+++ LivePreview/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  LivePreview
+//
+//  Created by Tanja Keune on 9/23/17.
+//  Copyright © 2017 SUGAPP. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
